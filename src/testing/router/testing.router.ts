c2c0af5a88c361326e372cd590401c31899bdd47
Alexander12081995 <a.parkouski.frontend@gmail.com>
2026-04-27 18:27:32 +0300

    fix authorization testing

--- src/testing/router/testing.router.ts
+++ src/testing/router/testing.router.ts
@@ -1,8 +1,6 @@
 import { Router } from "express";
-import { superAdminGuardMiddleware } from "../../auth/middlewares/super-admin.guard-middleware";
-import { inputValidationResultMiddleware } from "../../core/middlewares/validation/input-validation-result.middleware";
 import { cleanDBHandler } from "./handlers/cleanDB-handler";
 
 export const testingRouter = Router({});
 
-testingRouter.delete("", superAdminGuardMiddleware, inputValidationResultMiddleware, cleanDBHandler);
+testingRouter.delete("", cleanDBHandler);
